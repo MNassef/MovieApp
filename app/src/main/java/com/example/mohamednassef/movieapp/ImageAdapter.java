9ea92596b42237b97a70aad2e3e4e486cc8b8d8b
mohamednassef <mohamed.mmnassef@gmail.com>
2015-07-12 23:54:30 +0200

    Grid_Image_Text

--- app/src/main/java/com/example/mohamednassef/movieapp/ImageAdapter.java
+++ app/src/main/java/com/example/mohamednassef/movieapp/ImageAdapter.java
@@ -1,11 +1,12 @@
 package com.example.mohamednassef.movieapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by mohamednassef on 7/11/15.
@@ -16,6 +17,8 @@ public class ImageAdapter extends BaseAdapter {
     private Context mContext;
     // references to our images
     private Integer[] mThumbIds = { R.drawable.images3, R.drawable.images, R.drawable.images2};
+    private String [] mThumbtxt = {"Ocean_1","Ocean_2","Ocean_3"};
+
 
     public ImageAdapter(Context c) {
         mContext = c;
@@ -35,19 +38,30 @@ public class ImageAdapter extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
+        View grid;
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
+            //imageView = new ImageView(mContext);
+            /*
             imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
+            */
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            grid = inflater.inflate(R.layout.grid_image, null);
+            TextView textView = (TextView) grid.findViewById(R.id.text_view);
+            ImageView imageView = (ImageView) grid.findViewById(R.id.grid_image_view);
+            textView.setText(mThumbtxt[position]);
+            imageView.setImageResource(mThumbIds[position]);
+
+
+
         } else {
-            imageView = (ImageView) convertView;
+            grid = convertView;
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        //imageView.setImageResource(mThumbIds[position]);
+        return grid;
     }
 
 
